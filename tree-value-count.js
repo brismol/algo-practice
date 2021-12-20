0046137d2e40251eae741f4b68cc773ec0728cdf
Brian Smolin <briansmolin@yahoo.com>
2021-12-20 12:46:08 -0500

    tree-value-count

---
+++ tree-value-count.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const treeValueCount = (root, target) => {
+  if (!root) return 0;
+  const left = treeValueCount(root.left, target);
+  const right = treeValueCount(root.right, target);
+  const local = root.val === target ? 1 : 0;
+
+  return left + right + local;
+};
+
+const a = new Node(12);
+const b = new Node(6);
+const c = new Node(6);
+const d = new Node(4);
+const e = new Node(6);
+const f = new Node(12);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      12
+//    /   \
+//   6     6
+//  / \     \
+// 4   6     12
+
+console.log(treeValueCount(a, 6)); // -> 3
