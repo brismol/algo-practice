e5198852747fb6049c2882447ad3f9f0266f69bd
Brian Smolin <briansmolin@yahoo.com>
2021-12-30 16:44:10 -0500

    fib

---
+++ fib.js
@@ -0,0 +1,7 @@
+const fib = (n, memo = {}) => {
+  if (memo[n]) return memo[n];
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
